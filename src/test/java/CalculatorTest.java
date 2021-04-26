d78684f1eddbb6de4cd3684573cfc31abf53e14f
AKravchuk <kravchukodessa@gmail.com>
2021-04-26 18:20:31 +0300

    ignore failed tests

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,4 +1,5 @@
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class CalculatorTest {
@@ -11,24 +12,28 @@ public class CalculatorTest {
         Assert.assertEquals(5, calculator.calculateSumma(2, 3));
     }
 
+    @Ignore
     @Test
     public void testSumma1(){
         Calculator calculator = new Calculator();
         Assert.assertEquals(6, calculator.calculateSumma(2, 3));
     }
 
+    @Ignore
     @Test
     public void testSumma2(){
         Calculator calculator = new Calculator();
         Assert.assertEquals(7, calculator.calculateSumma(2, 3));
     }
 
+    @Ignore
     @Test
     public void testSumma3(){
         Calculator calculator = new Calculator();
         Assert.assertEquals(8, calculator.calculateSumma(2, 3));
     }
 
+    @Ignore
     @Test
     public void testSumma4(){
         Calculator calculator = new Calculator();
