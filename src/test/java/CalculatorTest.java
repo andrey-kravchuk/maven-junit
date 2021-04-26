eb9c5e96976d8661c185ca81ecfeebda140abdf5
AKravchuk <kravchukodessa@gmail.com>
2021-04-26 18:26:56 +0300

    one failed test

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -19,7 +19,7 @@ public class CalculatorTest {
         Assert.assertEquals(6, calculator.calculateSumma(2, 3));
     }
 
-    @Ignore
+//    @Ignore
     @Test
     public void testSumma2(){
         Calculator calculator = new Calculator();
