5677347907214b0c0556201a09143f2056440614
AKravchuk <kravchukodessa@gmail.com>
2021-05-11 19:10:46 +0300

    add comment 1

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,7 +3,7 @@ public class Calculator {
     public int calculateSumma(Integer a, Integer b) {
         return a + b;
     }
-
+    // add comment 1
     public int calculateDivide (Integer a, Integer b) {
         return a/b;
     }
