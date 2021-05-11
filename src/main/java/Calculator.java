281ac30efbfbcaf74aa50bd2a3428970838b8178
AKravchuk <kravchukodessa@gmail.com>
2021-05-11 19:03:01 +0300

    add divide method

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,4 +3,8 @@ public class Calculator {
     public int calculateSumma(Integer a, Integer b) {
         return a + b;
     }
+
+    public int calculateDivide (Integer a, Integer b) {
+        return a/b;
+    }
 }
