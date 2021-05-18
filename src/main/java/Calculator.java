4eb338b6c3d963476199ad7e9cc7f4405418c1ae
AKravchuk <kravchukodessa@gmail.com>
2021-05-18 17:11:32 +0300

    add long comment add long comment add long comment add long comment add long comment add long comment add long comment add long comment add long comment add long comment add long comment add long comment add long comment add long comment add long comment add long comment add long comment add long comment

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,10 +3,6 @@ public class Calculator {
     public int calculateSumma(Integer a, Integer b) {
         return a + b;
     }
-    // add comment 1
-    // add comment 2
-    // add comment 3
-    // add comment 4
     public int calculateDivide (Integer a, Integer b) {
         return a/b;
     }
